21c72f952ed2d7f50020d195ba4e640dbdc0ad1b
benjWood07 <dontjunkthis@gmail.com>
2025-07-21 22:18:31 -0500

    Explicit and implicit examples updated

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,23 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+let result = "5" - 2;  //this is already an example of an implicit conversion
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let implicitExample = "5" * 2;  //This is another example of an implicit coversion
+console.log(typeof implicitExample);  //Muliplication will implicity convert a string to a number
+console.log("The total in this exampe is " + implicitExample); 
+
+let isValid = Boolean(0);  //Edge case ex. where the Boolean value 0 evaluates to falsy 
 if (isValid) {
-    console.log("This is valid!");
+    console.log("This is valid!");  //This will not run
 }
 
 let age = "25";
-let totalAge = age + 5;
+console.log(typeof age);  //prints the current data type of the variable "age"
+let numAge = Number(age);  //explicity convert the variable "age" from string to a number
+console.log(typeof numAge)  //prints the current data type of the converted variable "age" to "numAge"
+let totalAge = numAge + 5;  //uses converted datatype version of "age" in the calculation
 console.log("Total Age: " + totalAge);
+
+
